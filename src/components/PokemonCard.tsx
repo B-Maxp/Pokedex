734526321415cfaa5943acccf2cacefce3b9dd3e
B-Maxp <max.bpr@outlook.fr>
2025-04-02 16:29:22 +0200

    :seedling: Add fist property

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,33 +1,11 @@
-// function PokemonCard() {
-//     return <figure>
-//         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-//         <figcaption>Bulbasaur</figcaption>
-//     </figure>
-// }
+function PokemonCard({ pokemon }) {
 
-// export default PokemonCard;
-
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
-
-function PokemonCard() {
-    const pokemon = pokemonList[1];
-
-    return <figure>
-        {pokemon.imgSrc !== undefined ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
-        <figcaption>{pokemon.name}</figcaption>
-
-    </figure>
-
-
+    return (
+        <figure>
+            {pokemon.imgSrc !== undefined ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    )
 }
 
 export default PokemonCard;
